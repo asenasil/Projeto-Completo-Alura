7d5c3a935e4bcc73e356fd31e7e31686c096f54e
asenasil <adalberto.sena.silva@everis.com>
2021-03-17 12:56:43 -0300

    Importando o projeto

---
+++ Alura Viagens/Home/View/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  Alura Viagens
+//
+//  Created by Adalberto Sena Silva on 17/03/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+   
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    @IBOutlet weak var imagemViagem: UIImageView!
+    
+}
